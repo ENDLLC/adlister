b6628f84f35972bd45a287b9accbe749485af42f
N Duke <duke9289@icloud.com>
2016-12-20 11:34:59 -0600

    add delete user account option

---
+++ src/main/java/com/codeup/adlister/controllers/DeleteUserServlet.java
@@ -0,0 +1,27 @@
+package com.codeup.adlister.controllers;
+
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Duke on 12/20/16.
+ */
+@WebServlet(name = "DeleteUserServlet", urlPatterns = "/userDelete")
+public class DeleteUserServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        User user = (User) request.getSession().getAttribute("user");
+        DaoFactory.getUsersDao().deleteUser(user);
+        response.sendRedirect("/logout");
+    }
+}
