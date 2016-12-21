8a98285479e6c4dd84c8c691561fed318a3c0383
N Duke <duke9289@icloud.com>
2016-12-21 14:08:46 -0600

    split update user and reset password into separate servlets

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -64,14 +64,25 @@ public class MySQLUsersDao implements Users {
     @Override
     public void update(User user) {
         try {
-            String updateQuery = "UPDATE users SET username = ?, email = ?, password = ? WHERE id = " + user.getId();
+            String updateQuery = "UPDATE users SET username = ?, email = ? WHERE id = " + user.getId();
             PreparedStatement stmt = connection.prepareStatement(updateQuery);
             stmt.setString(1, user.getUsername());
             stmt.setString(2, user.getEmail());
-            stmt.setString(3, user.getPassword());
             stmt.execute();
         } catch (SQLException e) {
             throw new RuntimeException("Error updating your information.", e);
+        }
+    }
+
+    @Override
+    public void resetPassword(User user) {
+        try {
+            String updateQuery = "UPDATE users SET password = ? WHERE id = " + user.getId();
+            PreparedStatement stmt = connection.prepareStatement(updateQuery);
+            stmt.setString(1, user.getPassword());
+            stmt.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException("Error changing your password.", e);
         }
     }
 
--- src/main/java/com/codeup/adlister/dao/Users.java
+++ src/main/java/com/codeup/adlister/dao/Users.java
@@ -6,5 +6,6 @@ public interface Users {
     User findByUsername(String username);
     Long insert(User user);
     void update(User user);
+    void resetPassword(User user);
     void deleteUser(User user);
 }
